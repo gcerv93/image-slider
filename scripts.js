7b73da4b7c0bdfb08140ebae69918cb365afc970
gcerv93 <gcerv93@protonmail.com>
2022-03-06 10:52:40 -0600

    working slider

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const images = document.querySelectorAll(".image");
+let num = 0;
 
 const hideImages = (idx) => {
   images.forEach((image, index) => {
@@ -9,4 +10,32 @@ const hideImages = (idx) => {
   });
 };
 
-hideImages(0);
+hideImages(num);
+
+const next = () => {
+  if (num == 4) {
+    return;
+  }
+  num += 1;
+  hideImages(num);
+  document.querySelector(`.image[data-index="${num}"]`).style.display = "block";
+};
+
+const previous = () => {
+  if (num == 0) {
+    return;
+  }
+  num -= 1;
+  hideImages(num);
+  document.querySelector(`.image[data-index="${num}"]`).style.display = "block";
+};
+
+const nextBtn = document.querySelector("#rightArrow");
+nextBtn.addEventListener("click", () => {
+  next();
+});
+
+const previousBtn = document.querySelector("#leftArrow");
+previousBtn.addEventListener("click", () => {
+  previous();
+});
