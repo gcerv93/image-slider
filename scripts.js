27de0eae3b0f42f62b9ef5e98b8845038715ca99
gcerv93 <gcerv93@protonmail.com>
2022-03-06 13:48:05 -0600

    wrap global variables in a module

--- scripts.js
+++ scripts.js
@@ -1,41 +1,45 @@
-const images = document.querySelectorAll(".image");
-let num = 0;
+const imageSlider = (() => {
+  const images = document.querySelectorAll(".image");
+  let num = 0;
 
-const hideImages = (idx) => {
-  images.forEach((image, index) => {
-    image.dataset.index = index;
-    if (index !== idx) {
-      image.style.display = "none";
+  const hideImages = (idx) => {
+    images.forEach((image, index) => {
+      image.dataset.index = index;
+      if (index !== idx) {
+        image.style.display = "none";
+      }
+    });
+  };
+
+  hideImages(num);
+
+  const next = () => {
+    if (num == 4) {
+      return;
     }
+    num += 1;
+    hideImages(num);
+    document.querySelector(`.image[data-index="${num}"]`).style.display =
+      "block";
+  };
+
+  const previous = () => {
+    if (num == 0) {
+      return;
+    }
+    num -= 1;
+    hideImages(num);
+    document.querySelector(`.image[data-index="${num}"]`).style.display =
+      "block";
+  };
+
+  const nextBtn = document.querySelector("#rightArrow");
+  nextBtn.addEventListener("click", () => {
+    next();
   });
-};
 
-hideImages(num);
-
-const next = () => {
-  if (num == 4) {
-    return;
-  }
-  num += 1;
-  hideImages(num);
-  document.querySelector(`.image[data-index="${num}"]`).style.display = "block";
-};
-
-const previous = () => {
-  if (num == 0) {
-    return;
-  }
-  num -= 1;
-  hideImages(num);
-  document.querySelector(`.image[data-index="${num}"]`).style.display = "block";
-};
-
-const nextBtn = document.querySelector("#rightArrow");
-nextBtn.addEventListener("click", () => {
-  next();
-});
-
-const previousBtn = document.querySelector("#leftArrow");
-previousBtn.addEventListener("click", () => {
-  previous();
-});
+  const previousBtn = document.querySelector("#leftArrow");
+  previousBtn.addEventListener("click", () => {
+    previous();
+  });
+})();
